c7ad85faadc1fc597af65dff6742bacd3113c520
Mary Ziemba <Mary@wl-10-190-69-98.wireless.duke.edu>
2016-10-11 11:35:15 -0400

    Updated main.py to 2016 season

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def getResults():
-    page = requests.get("http://www.goduke.com/SportSelect.dbml?SPSID=22851&SPID=1850&DB_LANG=C&DB_OEM_ID=4200&Q_SEASON=2015")
+    page = requests.get("http://www.goduke.com/SportSelect.dbml?SPSID=22851&SPID=1850&DB_OEM_ID=4200&Q_SEASON=2016")
+    # page = requests.get("http://www.goduke.com/SportSelect.dbml?SPSID=22851&SPID=1850&DB_OEM_ID=4200&Q_SEASON=2015")
     doc = BeautifulSoup(page.text, 'html.parser')
 
     results = doc.find_all("td", {"class" : "results"})
@@ -20,14 +21,26 @@ def getResults():
     # L = Duke Loss
     # C = Cancelled
     # P = Postponed
-    last_result = results[-1].get_text().strip().split()
-    last_outcome = last_result[0][0].upper()
 
-    ###### Opponent
-    last_opponent = opponents[-1].get_text().strip()
+    last_result = results[-1].get_text().strip().split()
+    if not results[0].get_text().strip().split():
+        last_result = []
+        last_outcome = "NOT YET"
+        last_opponent = ""
+    else:
+        last_result = getLastResult(results, -1)
+        last_outcome = last_result[0][0].upper()
+        last_opponent = opponents[-1].get_text().strip()
 
     rv = last_outcome + "\n" + last_opponent
     return rv
+
+def getLastResult(results, n):
+    result = results[n].get_text().strip().split()
+    if len(result) == 0:
+        getLastResult(results, n - 1)
+    return result
+
 
 if __name__ == "__main__":
     app.run()
